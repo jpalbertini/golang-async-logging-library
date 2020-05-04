2471c24a536e8ced86e123c32eac80cf15b6bf73
winux <jpalbertini@live.fr>
2020-05-04 22:56:06 +0200

    fix test

--- alog.go
+++ alog.go
@@ -54,12 +54,8 @@ func (al Alog) formatMessage(msg string) string {
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	al.m.Lock()
+	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
-	if recover() != nil {
-		al.m.Unlock()
-	} else {
-		al.m.Unlock()
-	}
 
 	if err != nil {
 		go func(e error) {
